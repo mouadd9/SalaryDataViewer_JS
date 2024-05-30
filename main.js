6a984813019bab2ffbdf1b682a4d07a5358c08b9
mouadispunk <bencaidmouad9@gmail.com>
2024-05-30 07:50:34 +0100

    added a script that simulates a database with two getter functions to fetsh for data

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 
 const companies = ["OpenAi", "X", "Meta"];
-const roles = ["Big data","AI" ];
+const roles = ["Big data", "AI"];
 
 // Create input buttons for every company and role represented in the data.
 
@@ -10,31 +10,35 @@ renderInputButtons(companies, 'company');
 renderInputButtons(roles, 'role');
 // This function will create a new <section> with radio
 // inputs based on the data provided in the labels array.
-function renderInputButtons(labels, groupName){
+function renderInputButtons(labels, groupName) {
     const container = document.createElement('section');
-    container.setAttribute('id',`${groupName}Inputs`);
+    container.setAttribute('id', `${groupName}Inputs`);
 
     let header = document.createElement('h3');
     header.innerText = `Select a ${groupName}`;
     container.appendChild(header);
 
     labels.forEach(label => {
+
         // Create the div holding the input and label elements.
         let divElement = document.createElement('div');
         divElement.setAttribute('class', 'option');
+
         // Create the radio input element.
         let inputElement = document.createElement('input');
         inputElement.setAttribute('type', 'radio');
         inputElement.setAttribute('name', groupName);
         inputElement.setAttribute('value', label);
         divElement.appendChild(inputElement);
+
         // Create a label for that radio input element.
         let labelElement = document.createElement('label');
         labelElement.setAttribute('for', label);
         labelElement.innerText = label;
         divElement.appendChild(labelElement);
 
-        //here it should be an event that will fire when clicking on an input 
+        // Update the results when the input is selected.
+        inputElement.addEventListener('click', updateResults);
 
         container.appendChild(divElement);
     });
@@ -42,3 +46,23 @@ function renderInputButtons(labels, groupName){
     document.querySelector('main').prepend(container);
 }
 
+
+function updateResults() {
+    // Get the current selected company and role from the radio button inputs.
+    const company = document.querySelector("input[name='company']:checked").value;
+    const role = document.querySelector("input[name='role']:checked").value;
+
+    if (!company || !role) {
+        return;
+    }
+
+    const averageSalaryByRole = 0;
+    const averageSalaryByCompany = 0;
+    const salary = 0;
+    const industryAverageSalary = 0;
+
+    document.getElementById('salarySelected').innerText = `The salary for ${role}s at ${company} is \$${salary}`;
+    document.getElementById('salaryAverageByRole').innerText = `The industry average salary for ${role} positions is \$${averageSalaryByRole}`;
+    document.getElementById('salaryAverageByCompany').innerText = `The average salary at ${company} is \$${averageSalaryByCompany}`;
+    document.getElementById('salaryAverageIndustry').innerText = `The average salary in the Tech industry is \$${industryAverageSalary}`;
+}
---
+++ salaryData.js
@@ -0,0 +1,40 @@
+// this simulates a data base 
+// each time we gather data from our checkboxes in main.js
+// we should fetch for the right object in this array 
+// and return it (it will contain our salary)
+const salaryData = [
+    { role: 'CTO', company: 'Big Data Inc.', salary: 320000 },
+    { role: 'Technical Lead', company: 'Big Data Inc.', salary: 230000 },
+    { role: 'Software Engineer II', company: 'Big Data Inc.', salary: 180000 },
+    { role: 'Software Engineer I', company: 'Big Data Inc.', salary: 140000 },
+    { role: 'CTO', company: 'Medium Data Inc.', salary: 215000 },
+    { role: 'Technical Lead', company: 'Medium Data Inc.', salary: 165000 },
+    { role: 'Software Engineer II', company: 'Medium Data Inc.', salary: 140000 },
+    { role: 'Software Engineer I', company: 'Medium Data Inc.', salary: 115000 },
+    { role: 'CTO', company: 'Small Data Inc.', salary: 175000 },
+    { role: 'Technical Lead', company: 'Small Data Inc.', salary: 135000 },
+    { role: 'Software Engineer II', company: 'Small Data Inc.', salary: 115000 },
+    { role: 'Software Engineer I', company: 'Small Data Inc.', salary: 95000 },
+];
+
+// before all this we need to get all the roles and companies to register them in our arrays back in main.js
+
+const getRoles = () => {
+    let roles = [];
+    salaryData.forEach(data => {
+        if (!roles.includes(data.role)) {
+            roles.push(data.role);
+        }
+    });
+    return roles;
+}
+
+const getCompanies = () => {
+    let companies = [];
+    salaryData.forEach(data => {
+        if (!companies.includes(data.company)) {
+            companies.push(data.company);
+        }
+    });
+    return companies;
+}
